a30508e7552477f3de21b0a31c5d37e84b32ce4d
Ryan-Mosier <ryan.m.mosier@gmail.com>
2025-12-21 23:42:58 -0700

    Remove `interpreter` target from CMake and update comments in source files for clarity.

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -2,6 +2,6 @@
 // Created by ryanm on 12/21/2025.
 //
 
-// this file will contain code to interpret a text file and create a expression tree w/ an environment
+// this file will contain code to interpret a text file and create an expression tree w/ an environment
 
 #include "Expressions/include/ExpressionType.hpp"
--- src/main.cpp
+++ src/main.cpp
@@ -2,3 +2,5 @@
 // Created by ryanm on 12/21/2025.
 //
 
+// This code will contain the main entry point for the program, handle UI (terminal for now) and manage program flow
+int main() { return 0; }
